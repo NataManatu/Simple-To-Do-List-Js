b05a99195513605ffffaafee9a8c48449dd243ae
NataR78 <100790826+NataR78@users.noreply.github.com>
2022-06-18 21:02:54 +0300

    To-Do List Js

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-let buttonTask = document.getElementById('button-task')
-let inputTask = document.getElementById('input-task')
-let tasktdl = document.getElementById('task-tdl')
+let buttonTask = document.getElementById('button-task');
+let inputTask = document.getElementById('input-task');
+let tasktdl = document.getElementById('task-tdl');
 
-function clickButton() {
+function clickButton(e) {
+    e.preventDefault()
+
     let newTasks = document.createElement('div');
     newTasks.classList.add('new-tasks');
-    newTasks.innerHTML = inputTask.value;
+
+
+    let checkbutton = document.createElement('button');
+    checkbutton.innerHTML = '<img src="tick.png" width="20px" height="20px"/>';
+    checkbutton.classList.add('check-button');
+    newTasks.appendChild(checkbutton);
+
+
+    let item = document.createElement('p');
+    item.classList.add('item');
+    item.innerText = inputTask.value;
+    newTasks.appendChild(item);
+
+    if (inputTask.value === '') return;
+
+    let trashbutton = document.createElement('button');
+    trashbutton.innerHTML = '<img src="delete.png" width="22px" height="22px"/>';
+    trashbutton.classList.add("trash-button");
+    newTasks.appendChild(trashbutton);
+
     tasktdl.appendChild(newTasks);
 
-     inputTask.value = ''
+    inputTask.value = ''
 
-
-
+    localStorage.setItem("task", JSON.stringify(inputTask.value))
+    console.log(localStorage)
 }
 
-buttonTask.addEventListener('click',clickButton)
+
+
+buttonTask.addEventListener('click', clickButton)
+
+function check_del(e) {
+    let item = e.target
+
+    if (item.classList[0] === 'check-button') {
+        let todolist = item.parentElement;
+        todolist.classList.toggle('checklist')
+    }
+
+    if (item.classList[0] === 'trash-button') {
+        const todolist = item.parentElement;
+        todolist.remove()
+    }
+}
+tasktdl.addEventListener('click', check_del)
